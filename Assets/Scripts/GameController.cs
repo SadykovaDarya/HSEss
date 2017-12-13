e2c6d91e992c231c4b1d9a39ea15499fa4970a2b
DESKTOP-8T57MJG\Дарья <lildeadwalrus@gmail.com>
2017-12-14 00:17:43 +0300

    Navigation from catch game is awesome! And it also has the end screen now yay

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour {
 
@@ -7,7 +8,11 @@ public class GameController : MonoBehaviour {
     public GameObject mark;
     private float maxWidth;
     public float timeleft;
-  
+    public Text timerText;
+    public GameObject gameOverText;
+    public GameObject restartButton;
+    public GameObject backToMainButton;
+
     void Start() {
         if (cam == null) {
             cam = Camera.main;
@@ -17,17 +22,28 @@ public class GameController : MonoBehaviour {
         float ballWidth = mark.GetComponent<SpriteRenderer>().bounds.extents.x;
         maxWidth = targetWidth.x - ballWidth;
         StartCoroutine(Spawn());
+        UpdateText();
     }
 
     void FixedUpdate() {
-         timeleft -= Time.deltaTime;
+        timeleft -= Time.deltaTime;
+        if (timeleft < 0)
+        {
+            timeleft = 0;
+        }
+        UpdateText();
         GameObject character = GameObject.FindGameObjectWithTag("Player");
         character.GetComponent<Rigidbody2D>().gravityScale = 1;
     }
 
+    void UpdateText()
+    {
+        timerText.text = "Time left: " + Mathf.RoundToInt(timeleft).ToString();
+    }
+
     public IEnumerator Spawn() {
        yield return new WaitForSeconds(2.0f);
-        while (timeleft>0) {
+        while (timeleft>0) { 
             Vector3 spawnPosition = new Vector3(
                Random.Range(-maxWidth, maxWidth),
                 transform.position.y, 0.0f
@@ -37,5 +53,9 @@ public class GameController : MonoBehaviour {
            
             yield return new WaitForSeconds(Random.Range(1.0f,2.0f));
         }
+        yield return new WaitForSeconds(1.0f);
+        gameOverText.SetActive(true);
+        restartButton.SetActive(true);
+        backToMainButton.SetActive(true);
     }
 }
