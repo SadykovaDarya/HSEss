6bf549007bde8f0d7d02d65d03ef183803c47f54
DESKTOP-505MN6E\Vanya <tominec98@gmail.com>
2017-12-14 22:40:53 +0300

    no message

---
+++ Assets/Scripts/Moving.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moving : MonoBehaviour {
+
+    public float speed = 3;
+    Camera cam;
+    // Use this for initialization
+    void Start () {
+        cam = Camera.main;
+	}
+
+    // Update is called once per frame
+    void FixedUpdate() {
+        float step = speed * Time.deltaTime;
+
+        if(GetComponent<CapsuleCollider2D>().transform.position.x + GetComponent<CapsuleCollider2D>().size.x * 2  > 
+            cam.transform.position.x - cam.orthographicSize) {
+                if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) {
+                    transform.position = new Vector3(transform.position.x - step, transform.position.y);
+                }
+        }
+
+        if (GetComponent<CapsuleCollider2D>().transform.position.x - GetComponent<CapsuleCollider2D>().size.x * 2<
+            cam.transform.position.x + cam.orthographicSize) {
+            if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) {
+                transform.position = new Vector3(transform.position.x + step, transform.position.y);
+            }
+        }
+    }
+}
