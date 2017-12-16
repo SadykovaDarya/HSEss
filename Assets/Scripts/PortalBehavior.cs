8cc382a1c586686f43d5a1a739ac328e9af236c4
DESKTOP-505MN6E\Vanya <tominec98@gmail.com>
2017-12-17 01:25:50 +0300

    ...

---
+++ Assets/Scripts/PortalBehavior.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalBehavior : MonoBehaviour {
+
+    public float activePlayers;
+    public float backGroundWidth;
+    public float playerspos;
+    public float targetspos;
+    GameObject clone;
+	// Update is called once per frame
+    void Start() {
+        backGroundWidth = GameObject.Find("background1").GetComponent<SpriteRenderer>().bounds.extents.x * 2;
+    }
+
+    void OnTriggerEnter2D(Collider2D collision) {
+        activePlayers = GameObject.FindGameObjectsWithTag("Player").Length;
+        if (activePlayers == 1) {
+            if (gameObject.transform.position.x > Camera.main.transform.position.x)
+                clone = Instantiate(gameObject, new Vector3(transform.position.x - backGroundWidth, transform.position.y), new Quaternion());
+            else
+                clone = Instantiate(gameObject, new Vector3(transform.position.x + backGroundWidth, transform.position.y), new Quaternion());
+            activePlayers++;
+            clone.GetComponent<Rigidbody2D>().velocity = gameObject.GetComponent<Rigidbody2D>().velocity;
+        }
+    }
+
+    void FixedUpdate() {
+        if(clone != null){
+            clone.transform.position = new Vector3(clone.transform.position.x, transform.position.y);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision) {
+        if (activePlayers == 2) {
+            if (transform.position.x < Camera.main.transform.position.x - backGroundWidth / 2 || 
+                transform.position.x > Camera.main.transform.position.x + backGroundWidth / 2) {
+                Destroy(gameObject);
+                activePlayers--;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/SpawnPlatforms.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPlatforms : MonoBehaviour {
+    //meme
+    public GameObject platform;
+    public Vector3 position;
+    float maxWidth;
+    List<GameObject> platforms;
+
+    void Start () {
+        platform = GameObject.FindWithTag("Platform");
+        platforms = new List<GameObject>() { platform };
+        Vector3 upperCorner = new Vector3(Screen.width, Screen.height, 0.0f);
+        Vector3 targetWidth = Camera.main.ScreenToWorldPoint(upperCorner);
+        maxWidth = targetWidth.x - platform.GetComponent<SpriteRenderer>().bounds.extents.x;
+        position = new Vector3(Random.Range(-maxWidth, maxWidth), platform.transform.position.y, 0f);    
+	    for(int i=0; i<5; i++) {
+            UpdatePosition();
+            platforms.Add(Instantiate(platform, position, new Quaternion()));
+        }
+	}
+
+    void Update() {
+        foreach(var plat in platforms) {
+            if (plat.transform.position.y < Camera.main.transform.position.y - Camera.main.orthographicSize)
+                plat.transform.position = UpdatePosition();
+        }
+    }
+
+    Vector3 UpdatePosition(){
+        return position = new Vector3(Random.Range(-maxWidth, maxWidth), position.y + Random.Range(.8f, 1.2f), 0f);
+    }
+}
